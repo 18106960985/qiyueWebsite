7190676af4133334a188a6c06a878e3767876a12
yebinghuan <747718944@qq.com>
2018-03-13 15:38:41 +0800

    首页轮播图和控制器

---
+++ src/views/Carousel/components/pageController.vue
@@ -0,0 +1,125 @@
+<template>
+  <div>
+      <!--两侧左右按钮 使用绝对布局-->
+      <div class="banner-ctrl">
+        <div class="ctrl-box ctrl-left"><svg-icon icon-class="qq-2"/></div>
+        <div class="ctrl-box ctrl-right"><svg-icon icon-class="rgihtPage"/></div>
+      </div>
+      <!--轮播图导航条-->
+      <div class="banner-page container-horizontal pagination-bullets">
+            <span class="pagination-bullet active">
+
+            </span>
+        <span class="pagination-bullet">
+
+            </span>
+        <span class="pagination-bullet">
+
+            </span>
+        <span class="pagination-bullet">
+
+            </span>
+
+      </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'page-controller',
+
+  }
+</script>
+
+<style scoped>
+  * {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    z-index: 1;
+  }
+  div {
+    display: block;
+  }
+
+  .banner-ctrl{
+
+  }
+  .banner-ctrl .ctrl-box {
+    position: absolute;
+    color: black;
+    width: 80px;
+    text-align: center;
+    line-height: 80px;
+    font-size: 50px;
+    cursor: pointer;
+    transition: .3s;
+    -webkit-transition: .3s;
+    -moz-transition: .3s;
+    -ms-transition: .3s;
+    -o-transition: .3s;
+  }
+  .ctrl-box.ctrl-left{
+    left: 5%;
+    top: 46%;
+    margin-top: -40px;
+  }
+  .ctrl-box.ctrl-right{
+    right: 5%;
+    top: 46%;
+    margin-top: -40px;
+  }
+
+  .ctrl-box {
+    opacity: .5;
+  }
+  /*分页*/
+  .banner-page{
+    position: absolute;
+    height: 3px;
+    line-height: 0;
+    top: 84%;
+    bottom: auto !important;
+    left: 0;
+    right: 0;
+    text-align: center;
+  }
+
+  .banner-page .pagination-bullet{
+    width: 40px;
+    height: 3px;
+    position: relative;
+    line-height: 3px;
+    border-radius: 0;
+    background: #fff;
+    opacity: 1;
+  }
+
+  .pagination-clickable .pagination-bullet {
+    cursor: pointer;
+  }
+  .pagination-bullet {
+    width: 8px;
+    height: 8px;
+    display: inline-block;
+    border-radius: 100%;
+    background: #fff;
+    opacity: .2;
+    margin: 0 5px;
+  }
+
+  .pagination-bullet.active{
+    background: #ff9900;
+  }
+
+  .pagination-bullet:before {
+      position: absolute;
+      width: 40px;
+      height: 30px;
+      content: '';
+      left: 0;
+      top: -10px;
+    }
+
+
+</style>
---
+++ src/views/Carousel/index.vue
@@ -0,0 +1,98 @@
+<template>
+
+  <div>
+     <ye-page :currentPage="currentPage" :imgSrc="components.imgSrc" :direction="direction" :pageIndex="1" :isCenter="true">
+       <h1 class="text-center">项目介绍</h1>
+       <section class="animate" ref="section1">
+         <p class="demo-intro">Vue-FullPage是一个使用Vue制作的全屏页面模板，是为了学习Vue而制作的一个Vue项目</p>
+         <p class="demo-intro">使用到的相关npm包有：
+           <a href="https://github.com/vuejs/vue-cli" target="_blank">vue-cli</a>、
+           <a href="https://github.com/vuejs-templates/webpack-simple" target="_blank">webpack-simple模板</a>
+         </p>
+         <p class="demo-intro">支持：导航条滚动，滚轮滚动，触摸滚动</p>
+       </section>
+     </ye-page>
+
+    <ye-page :currentPage="currentPage" :direction="direction" :pageIndex="2">
+
+    </ye-page>
+    <ye-page :currentPage="currentPage" :direction="direction" :pageIndex="3">
+
+    </ye-page>
+    <ye-page :currentPage="currentPage" :direction="direction" :pageIndex="4">
+
+    </ye-page>
+    <ye-page :currentPage="currentPage" :direction="direction" :pageIndex="5">
+
+    </ye-page>
+    <page-controller  ></page-controller>
+
+  </div>
+</template>
+
+<script>
+  import yeCarousel from "../../components/Carousel/yeCardPage"
+  import pageController from "./components/pageController"
+
+  // 页面进出动画
+  function afterEnterAnimate($child) {
+    $child.$el.querySelector('.animate').classList.remove('move-left', 'move-right');
+  }
+  function beforeLeaveAnimate($child) {
+    let moveType = Math.random() > 0.5 ? 'move-left' : 'move-right';
+    $child.$el.querySelector('.animate').classList.add(moveType);
+  }
+    export default {
+        name: "index",
+      components:{
+        "ye-page":yeCarousel,
+        "page-controller":pageController,
+      },
+      data(){
+          return{
+            currentPage: 1,
+            direction:"x",
+            options: [{
+              // the color of background
+              background: 'rgba(229, 199, 46, 1)',
+              // the color of text
+              color: '#fff',
+              // is content center
+              isCenter: true,
+              // the function before page show
+              afterEnter: afterEnterAnimate,
+              // the function after page show
+              beforeLeave: beforeLeaveAnimate
+            }, {
+              background: 'rgba(79, 204, 76, 1)',
+              color: '#fff',
+              isCenter: true,
+              afterEnter: afterEnterAnimate,
+              beforeLeave: beforeLeaveAnimate
+            }, {
+              background: 'rgba(233, 84, 84, 1)',
+              color: '#fff',
+              isCenter: true,
+              afterEnter: afterEnterAnimate,
+              beforeLeave: beforeLeaveAnimate
+            }, {
+              background: 'rgba(46, 153, 229, 1)',
+              color: '#fff',
+              isCenter: true,
+              afterEnter: afterEnterAnimate,
+              beforeLeave: beforeLeaveAnimate
+            }],
+            components:{
+              imgSrc:"https://images.apple.com/v/mac/home/y/images/home/imac_pro_large_2x.jpg",
+            }
+          }
+      },
+      computed:{
+
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
