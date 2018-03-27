8009f9b1c2284ac32c3c9666c160e5c7cee40640
yebinghuan <747718944@qq.com>
2018-03-27 11:23:54 +0800

    组件完成。开始配置路由

---
+++ src/views/Layout/Layout.vue
@@ -0,0 +1,62 @@
+<!--整体页面布局-->
+<template>
+  <div >
+    <div class="main-container">
+      <meta-left-nav-menu :isActive="leftMenuIsActive"></meta-left-nav-menu>
+      <keep-alive >
+        <router-view></router-view>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+  import metaIndex from  "../index/index";
+  import leftMenu from "./components/metaLeftMenu";
+
+  import metaDetails from '../companyNews/metaDetails'
+
+  export default {
+    name: "layout",
+    components:{
+      'ye-index':metaIndex,
+      'meta-left-nav-menu': leftMenu,
+      'test' :metaDetails,
+    },
+    data(){
+      return {
+        leftMenuIsActive:false,
+
+
+      }
+    },
+    methods:{
+
+    }
+  }
+</script>
+
+<style scoped>
+  *{
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #fff;
+  }
+
+  .app {
+    height: 100%;
+    width: 100%;
+  }
+</style>
