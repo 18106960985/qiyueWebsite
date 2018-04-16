0d5b786b1d2e0553cef6ee0d40b1a0bf003067ba
y747718944 <747718944@qq.com>
2018-04-16 10:08:31 +0800

    升级webpack 版本 ，手机移动有问题

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api':{
-        target: 'http://top.meta.net.cn:88',
+        target: 'http://top.meta.net.cn',
         changeOrigin: true,
         pathRewrite: {
           '^/api': '/api'
